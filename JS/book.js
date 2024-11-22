810c692ee1f315fc0aeaacdce149379d7d6faea1
Shakil Ahmed <shakil200409@gmail.com>
2024-11-23 00:36:41 +0600

    Edited About page

---
+++ JS/book.js
@@ -0,0 +1,10 @@
+// showBook()
+const bookPage= document.getElementById('books-page');
+
+console.log(bookPage)
+function showBook(){
+    console.log('clicked')
+    bookPage.forEach(element => {
+        // console.log(element)
+    });
+}
